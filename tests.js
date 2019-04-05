86df95a1318a00dd220da9c1d6fe6d0134ee5d1a
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-05 12:29:41 -0500

    add the third and fourth test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -18,6 +18,12 @@ describe('sayHello',function(){
         expect(typeof sayHello).toBe('function');
     });
     it('should return a string when called',function(){
-        expect(typeof sayHello()).toBe("string")
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return "Hello, Jane!"',function(){
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!"',function(){
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
 });
