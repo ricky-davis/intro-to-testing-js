1eb117b2d7b9a3855330e88a0dcb379a3fc6c060
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-05 13:17:15 -0500

    add the sixth test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -3,11 +3,5 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 function sayHello(input){
-    if (input==="Alex"){
-        return "Hello, Alex!";
-    }else if (input==="Pat"){
-        return "Hello, Pat!";
-    }else{
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
 }
